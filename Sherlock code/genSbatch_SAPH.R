87081382d5b59452ab6ffecc093d1c10250746e8
Maya Mathur <mmathur@stanford.edu>
2022-03-24 06:30:17 -0700

    while sims run, look again at empirical stats from SAPBE metas to inform sim params

--- Sherlock code/genSbatch_SAPH.R
+++ Sherlock code/genSbatch_SAPH.R
@@ -203,7 +203,7 @@ n.files
 # 3840
 path = "/home/groups/manishad/SAPH"
 setwd( paste(path, "/sbatch_files", sep="") )
-for (i in 3001:3840) {
+for (i in 1:45) {
   system( paste("sbatch -p qsu,owners,normal /home/groups/manishad/SAPH/sbatch_files/", i, ".sbatch", sep="") )
 }
 
